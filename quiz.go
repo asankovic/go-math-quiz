9f8ffa7f39a7befbf43ee898444c9397ffa6f3f7
Alen Sankovic <alen.sankovic@ibmix.hr>
2023-08-25 13:47:21 +0200

    add replay and shuffle option

--- quiz.go
+++ quiz.go
@@ -1,29 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+)
 
-//TODO add timer, shuffle and replay options
+//TODO add timer
+//TODO add regex check for custom csv questions
 
 func runQuiz(tasks []quizTask, options quizOptions) {
 	correct := 0
+
 	numQuestions := *options.quantity
 	if numQuestions > len(tasks) {
 		numQuestions = len(tasks)
 	}
 
+	selectedTasks := tasks[:numQuestions]
+	if *options.shuffle {
+		rand.Shuffle(len(selectedTasks), func(i, j int) { selectedTasks[i], selectedTasks[j] = selectedTasks[j], selectedTasks[i] })
+	}
+
 	fmt.Printf("Starting level %d quiz with %d questions, good luck!\n", *options.level, numQuestions)
-	for i, task := range tasks[:numQuestions] {
+	for i, task := range selectedTasks {
 		fmt.Println("===========================================")
 		fmt.Printf("Question #%d: %s", i+1, task.question)
-		if evaluateUser(task) {
+		if correctUserAnswer(task) {
 			correct++
 		}
 	}
 	fmt.Println("===========================================")
-	fmt.Printf("Quiz finished, your score was %d/%d!\n", correct, len(tasks))
+	fmt.Printf("Quiz finished, your score was %d/%d!\n", correct, numQuestions)
 }
 
-func evaluateUser(task quizTask) bool {
+func correctUserAnswer(task quizTask) bool {
 	var userAnswer string
 	fmt.Scan(&userAnswer)
 	if userAnswer == task.answer {
